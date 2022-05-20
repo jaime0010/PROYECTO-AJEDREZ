c0f0bbe0dc899035e6022567cab275d4d953c704
jaime0010 <80421595+jaime0010@users.noreply.github.com>
2022-05-21 01:52:56 +0200

    Update listapiezas.h

--- listapiezas.h
+++ listapiezas.h
@@ -9,12 +9,18 @@ class listapiezas
 public:
 	int numero_p;
 	listapiezas();
+	int x_fuera = 9, y_fuera = 1;
     Pieza* lista_piezas[MAX_PIEZAS];
 	bool agregar(Pieza*);
 	void dibuja();
 
 	void mover(int h);
 	int donde_esta(Vector2D);
+	
+	void destruir(int);
+
+	bool es_blanca(int);
+	bool validar_peon(Vector2D,int);
 
    
 };
