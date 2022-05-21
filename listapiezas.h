392e4a6de6a466b84d3d4fbdbdc1b6a065065bbd
Victor-Alcolea <100533464+Victor-Alcolea@users.noreply.github.com>
2022-05-21 13:56:48 +0200

    Update listapiezas.h

--- listapiezas.h
+++ listapiezas.h
@@ -1,6 +1,10 @@
 #pragma once
 #include"peon.h"
 #include "Torre.h"
+#include"Alfil.h"
+#include"Rey.h"
+#include"Reina.h"
+#include"Caballo.h
 #define MAX_PIEZAS 32
 
 
@@ -9,7 +13,7 @@ class listapiezas
 public:
 	int numero_p;
 	listapiezas();
-	int x_fuera = 9, y_fuera = 1;
+	int x_fuera = 11, y_fuera = 1;
     Pieza* lista_piezas[MAX_PIEZAS];
 	bool agregar(Pieza*);
 	void dibuja();
@@ -21,6 +25,15 @@ public:
 
 	bool es_blanca(int);
 	bool validar_peon(Vector2D,int);
+	
+        bool validar_torre(Vector2D, int);
 
+	bool validar_alfil(Vector2D, int);
+
+	bool validar_rey(Vector2D, int);
+
+	bool validar_reina(Vector2D, int);
+
+	bool validar_caballo(Vector2D, int);
    
 };
