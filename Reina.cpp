b54816da610dbd77377dd8689746da4289a09825
Victor-Alcolea <100533464+Victor-Alcolea@users.noreply.github.com>
2022-05-22 13:25:09 +0200

    Update Reina.cpp

--- Reina.cpp
+++ Reina.cpp
@@ -1,5 +1,6 @@
 #include "Reina.h"
 #include "freeglut.h"
+#include"ETSIDI.h"
 
 Reina::Reina(float x, float y, float r, int c)
 {
@@ -12,9 +13,94 @@ Reina::Reina(float x, float y, float r, int c)
 
 void Reina::dibuja()
 {
+    int k, j;
+    j = (int)posicion.x % 2;
+    k = (int)posicion.y % 2;
+
+    if (color == 0)
+    {
+
+        if (j == 0)
+        {
+            if (k == 0)
+                glColor3ub(46, 97, 158);//color azul oscuro
+            else
+                glColor3ub(156, 198, 249);//color azul claro
+
+        }
+        else if (j == 1)
+        {
+            if (k == 0)
+                glColor3ub(156, 198, 249);//color azul claro
+
+            else
+                glColor3ub(46, 97, 158);//color azul oscuro
+        }
+
+        glEnable(GL_TEXTURE_2D);         glBindTexture(GL_TEXTURE_2D, ETSIDI::getTexture("reina-negrap.png").id);
+        glDisable(GL_LIGHTING);
+        glBegin(GL_POLYGON);
+
+
+        glTexCoord2d(0, 1); ;glVertex3f(posicion.x - 0.5, posicion.y - 0.5, 0.01f);
+        glTexCoord2d(1, 1); ;glVertex3f(posicion.x + 0.5, posicion.y - 0.5, 0.01f);
+        glTexCoord2d(1, 0);   glVertex3f(posicion.x + 0.5, posicion.y + 0.5, 0.01f);
+        glTexCoord2d(0, 0); glVertex3f(posicion.x - 0.5, posicion.y + 0.5, 0.01f);
+        glEnd();
+        glEnable(GL_LIGHTING);
+
+        //Liberar memoria de la textura
+        glBindTexture(GL_TEXTURE_2D, 0);
+        glColor3ub(color, color, color);
+        glTranslatef(posicion.x, posicion.y, 0);
+        //glutSolidSphere(radio, 20, 20);
+        glTranslatef(-posicion.x, -posicion.y, 0);
+    }
+    if (color == 255)
+    {
+
+
+        if (j == 0)
+        {
+            if (k == 0)
+                glColor3ub(46, 97, 158);//color azul oscuro
+            else
+                glColor3ub(156, 198, 249);//color azul claro
+
+        }
+        else if (j == 1)
+        {
+            if (k == 0)
+                glColor3ub(156, 198, 249);//color azul claro
+
+            else
+                glColor3ub(46, 97, 158);//color azul oscuro
+        }
+
+
+        glEnable(GL_TEXTURE_2D);         glBindTexture(GL_TEXTURE_2D, ETSIDI::getTexture("reina-blancap.png").id);
+        glDisable(GL_LIGHTING);
+        glBegin(GL_POLYGON);
+
+        glTexCoord2d(0, 1); ;glVertex3f(posicion.x - 0.5, posicion.y - 0.5, 0.01f);
+        glTexCoord2d(1, 1); ;glVertex3f(posicion.x + 0.5, posicion.y - 0.5, 0.01f);
+        glTexCoord2d(1, 0);   glVertex3f(posicion.x + 0.5, posicion.y + 0.5, 0.01f);
+        glTexCoord2d(0, 0); glVertex3f(posicion.x - 0.5, posicion.y + 0.5, 0.01f);
+
+        glEnd();
+        glEnable(GL_LIGHTING);
+
+        //Liberar memoria de la textura
+        glBindTexture(GL_TEXTURE_2D, 0);
+        glColor3ub(color, color, color);
+        glTranslatef(posicion.x, posicion.y, 0);
+        //glutSolidSphere(radio, 20, 20);
+        glTranslatef(-posicion.x, -posicion.y, 0);
+
+    }
     glColor3ub(color, color, color);
     glTranslatef(posicion.x, posicion.y, 0);
-    glutWireCone(radio, 2, 10,10);
+   // glutWireCone(radio, 2, 10,10);
     glTranslatef(-posicion.x, -posicion.y, 0);
 
 }
