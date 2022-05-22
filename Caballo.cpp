b87da3a7db3ab6e9baa27b1f5665d5c83500cf56
Victor-Alcolea <100533464+Victor-Alcolea@users.noreply.github.com>
2022-05-22 13:24:28 +0200

    Update Caballo.cpp

--- Caballo.cpp
+++ Caballo.cpp
@@ -1,5 +1,6 @@
 #include "Caballo.h"
 #include "freeglut.h"
+#include"ETSIDI.h"
 
 Caballo::Caballo(float x, float y, float r, int c)
 {
@@ -12,9 +13,93 @@ Caballo::Caballo(float x, float y, float r, int c)
 
 void Caballo::dibuja()
 {
+    int k, j;
+    j = (int)posicion.x % 2;
+    k = (int)posicion.y % 2;
+
+    if (color == 0)
+    {
+
+        if (j == 0)
+        {
+            if (k == 0)
+                glColor3ub(46, 97, 158);//color azul oscuro
+            else
+                glColor3ub(156, 198, 249);//color azul claro
+
+        }
+        else if (j == 1)
+        {
+            if (k == 0)
+                glColor3ub(156, 198, 249);//color azul claro
+
+            else
+                glColor3ub(46, 97, 158);//color azul oscuro
+        }
+
+        glEnable(GL_TEXTURE_2D);         glBindTexture(GL_TEXTURE_2D, ETSIDI::getTexture("caballo-negrop.png").id);
+        glDisable(GL_LIGHTING);
+        glBegin(GL_POLYGON);
+
+
+        glTexCoord2d(0, 1); ;glVertex3f(posicion.x - 0.5, posicion.y - 0.5, 0.01f);
+        glTexCoord2d(1, 1); ;glVertex3f(posicion.x + 0.5, posicion.y - 0.5, 0.01f);
+        glTexCoord2d(1, 0);   glVertex3f(posicion.x + 0.5, posicion.y + 0.5, 0.01f);
+        glTexCoord2d(0, 0); glVertex3f(posicion.x - 0.5, posicion.y + 0.5, 0.01f);
+        //
+        glEnd();
+        glEnable(GL_LIGHTING);
+
+        //Liberar memoria de la textura
+        glBindTexture(GL_TEXTURE_2D, 0);
+        glColor3ub(color, color, color);
+        glTranslatef(posicion.x, posicion.y, 0);
+        //glutSolidSphere(radio, 20, 20);
+        glTranslatef(-posicion.x, -posicion.y, 0);
+    }
+    if (color == 255)
+    {
+
+        if (j == 0)
+        {
+            if (k == 0)
+                glColor3ub(46, 97, 158);//color azul oscuro
+            else
+                glColor3ub(156, 198, 249);//color azul claro
+                
+        }
+        else if (j == 1)
+        {
+            if (k == 0)
+                glColor3ub(156, 198, 249);//color azul claro
+
+            else
+                glColor3ub(46, 97, 158);//color azul oscuro
+        }
+
+        glEnable(GL_TEXTURE_2D);         glBindTexture(GL_TEXTURE_2D, ETSIDI::getTexture("caballo-blancop.png").id);
+        glDisable(GL_LIGHTING);
+        glBegin(GL_POLYGON);
+
+        glTexCoord2d(0, 1); ;glVertex3f(posicion.x - 0.5, posicion.y - 0.5, 0.01f);
+        glTexCoord2d(1, 1); ;glVertex3f(posicion.x + 0.5, posicion.y - 0.5, 0.01f);
+        glTexCoord2d(1, 0);   glVertex3f(posicion.x + 0.5, posicion.y + 0.5, 0.01f);
+        glTexCoord2d(0, 0); glVertex3f(posicion.x - 0.5, posicion.y + 0.5, 0.01f);
+
+        glEnd();
+        glEnable(GL_LIGHTING);
+
+        //Liberar memoria de la textura
+        glBindTexture(GL_TEXTURE_2D, 0);
+        glColor3ub(color, color, color);
+        glTranslatef(posicion.x, posicion.y, 0);
+        //glutSolidSphere(radio, 20, 20);
+        glTranslatef(-posicion.x, -posicion.y, 0);
+
+    }
     glColor3ub(color, color, color);
     glTranslatef(posicion.x, posicion.y, 0);
-    glutWireCube(radio);
+    //glutWireCube(radio);
     glTranslatef(-posicion.x, -posicion.y, 0);
 
 }
