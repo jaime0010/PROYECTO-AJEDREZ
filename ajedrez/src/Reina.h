1f8c784ec3be4a379a095e942dc513eef2dc41da
Felipedegracia <100533450+Felipedegracia@users.noreply.github.com>
2022-07-09 17:33:37 +0200

    Update Reina.h

--- ajedrez/src/Reina.h
+++ ajedrez/src/Reina.h
@@ -6,7 +6,7 @@ class Reina : public Pieza //una Reina es una Pieza
 public:
 
 	//Constructor
-	Reina(Vector2D pos, float r, bool b);
+	Reina(Vector2D pos, bool b);
 
 	//Métodos (heredados de la clase Pieza)
 	void dibuja();
