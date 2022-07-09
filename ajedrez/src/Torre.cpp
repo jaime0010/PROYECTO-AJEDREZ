206228849a569a6fa01813f6d1714cf33170b85d
Felipedegracia <100533450+Felipedegracia@users.noreply.github.com>
2022-07-09 17:33:07 +0200

    Update Torre.cpp

--- ajedrez/src/Torre.cpp
+++ ajedrez/src/Torre.cpp
@@ -2,7 +2,7 @@
 #include "freeglut.h"
 #include "ETSIDI.h"
 
-Torre::Torre(Vector2D pos, float r, bool b) : Pieza(pos, r, b) //Constructor
+Torre::Torre(Vector2D pos,bool b) : Pieza(pos, b) //Constructor
 {
 
 }
