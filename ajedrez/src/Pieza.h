9e20307291db3e093993b62b1a39ec87960eb54b
pedrodemartino <100533440+pedrodemartino@users.noreply.github.com>
2022-07-09 16:24:05 +0200

    get_blanca()
    Método para tener bool blanca en PROTECTED

--- ajedrez/src/Pieza.h
+++ ajedrez/src/Pieza.h
@@ -9,13 +9,12 @@ class ListaPiezas;
 class Pieza
 {
 protected:
-	//bool blanca;
+	bool blanca;
 	//Vector2D posicion; //creo que debe ser protected!!!!
 public:
 	//Atributos
 	Vector2D posicion;
 	float radio;
-	bool blanca;
 
 
 	//Constructor
@@ -26,5 +25,8 @@ public:
 	virtual void dibuja() = 0; //Método virtual puro -> Pieza == clase abstracta (no instancia concreta)
 	virtual bool validar_movimiento(Vector2D, ListaPiezas*) = 0; //Método virtual puro -> Pieza == clase abstracta (no instancia concreta)
 	bool mismo_color(Pieza*);
+	
+	//Métodos para tener los atributos en PROTECTED/PRIVADO
+	bool get_blanca();
 
 };
